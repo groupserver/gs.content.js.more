fcc8659a627373994d7d4ae1a1a0150b10993086
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-11 10:20:31 +1100

    Using constants in the JS, and making the class names the same as the documentation.

--- gs/content/js/more/browser/javascript/more.js
+++ gs/content/js/more/browser/javascript/more.js
@@ -8,21 +8,24 @@ var GSMoreWidget = function (widgetId) {
     var moreButton = null;
     var lessButton = null;
 
+    var LARGE = 'gs-content-js-more-content-large';
+    var SMALL = 'gs-content-js-more-content-small';
+
     var compress = function() {
-        content.removeClass('gs-content-js-more-content-large');
-        content.addClass('gs-content-js-more-content-small');
+        content.removeClass(LARGE);
+        content.addClass(SMALL);
         moreButton.show();
         lessButton.hide();
     };
     var enlarge = function() {
-        content.removeClass('gs-content-js-more-content-small');
-        content.addClass('gs-content-js-more-content-large');
+        content.removeClass(SMALL);
+        content.addClass(LARGE);
         moreButton.hide();
         lessButton.show();
     };
 
     var handle_click = function(event, data) {
-        if (content.hasClass('gs-content-js-more-content-small')) {
+        if (content.hasClass(SMALL)) {
             enlarge();
         } else {
             compress();
@@ -33,13 +36,13 @@ var GSMoreWidget = function (widgetId) {
         widget = jQuery(widgetId);
 
         content = widget.find('.gs-content-js-more-content');
-        moreButton = widget.find('.gs-content-js-more-button');
+        moreButton = widget.find('.gs-content-js-more-buttons-more');
 
         moreButton.removeAttr('href');
         moreButton.css('cursor', 'pointer');
         moreButton.click(handle_click);
 
-        lessButton = widget.find('.gs-content-js-less-button');
+        lessButton = widget.find('.gs-content-js-more-buttons-less');
         lessButton.removeAttr('href');
         lessButton.css('cursor', 'pointer');
         lessButton.click(handle_click);
