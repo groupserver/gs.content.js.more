0479f788c7af37994a8d085ea16d186f4d76621a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-06 10:46:09 +1100

    Hiding the More button when there is less than four lines showning.

--- gs/content/js/more/browser/javascript/more.js
+++ gs/content/js/more/browser/javascript/more.js
@@ -11,20 +11,20 @@ var GSMoreWidget = function (widgetId) {
     var LARGE = 'gs-content-js-more-content-large';
     var SMALL = 'gs-content-js-more-content-small';
 
-    var compress = function() {
+    function compress() {
         content.removeClass(LARGE);
         content.addClass(SMALL);
         moreButton.show();
         lessButton.hide();
     };
-    var enlarge = function() {
+    function enlarge() {
         content.removeClass(SMALL);
         content.addClass(LARGE);
         moreButton.hide();
         lessButton.show();
     };
 
-    var handle_click = function(event, data) {
+    function handle_click(event, data) {
         if (content.hasClass(SMALL)) {
             enlarge();
         } else {
@@ -32,21 +32,29 @@ var GSMoreWidget = function (widgetId) {
         }
     }
 
-    var init = function () {
+    function init() {
         widget = jQuery(widgetId);
 
         content = widget.find('.gs-content-js-more-content');
         moreButton = widget.find('.gs-content-js-more-buttons-more');
-
-        moreButton.removeAttr('href');
-        moreButton.css('cursor', 'pointer');
-        moreButton.click(handle_click);
-
         lessButton = widget.find('.gs-content-js-more-buttons-less');
-        lessButton.removeAttr('href');
-        lessButton.css('cursor', 'pointer');
-        lessButton.click(handle_click);
 
-        compress();
-    }(); // init. Note the () is deliberate, so it is run automatically.
+        enlarge();
+
+        if (content.height() > 80) {
+            moreButton.removeAttr('href');
+            moreButton.css('cursor', 'pointer');
+            moreButton.click(handle_click);
+            
+            lessButton.removeAttr('href');
+            lessButton.css('cursor', 'pointer');
+            lessButton.click(handle_click);
+
+            compress();
+        } else { // content.height() <= 80
+            widget.find('.gs-content-js-more-buttons').hide();
+        }
+    }
+
+    init(); // Run the init automatically.
 };
