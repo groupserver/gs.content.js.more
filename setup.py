d1f5ac52af5c3089a7a28becf51300f0a56975e4
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-30 16:32:14 -0700

    Following jQuery to its new home

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
 
@@ -25,7 +26,7 @@ setup(name='gs.content.js.more',
       install_requires=[
           'setuptools',
           'zope.browserresource',
-          'gs.content.js.jquery',
+          'gs.content.js.jquery.base',
       ],
       entry_points="""
       # -*- Entry points: -*-
