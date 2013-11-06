3aa0c51843f975c8fa2fbf1b7edc865bbdfd710a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-06 15:54:54 -0800

    Not zip_safe.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='gs.content.js.more',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['gs'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
           'setuptools',
           'gs.content.js.jquery.base',
